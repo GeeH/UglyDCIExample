571b390fa7cffdc4a6fa176ed0742bf96086ebc0
Gary Hockin <its.spabby@gmail.com>
2013-07-28 09:24:05 +0100

    uncommented commented tests

--- spec/Ugly/DCI/Context/ContextAbstractSpec.php
+++ spec/Ugly/DCI/Context/ContextAbstractSpec.php
@@ -1,37 +1,37 @@
 <?php
-//
-//namespace spec\Ugly\DCI\Context;
-//
-//use PhpSpec\ObjectBehavior;
-//use Prophecy\Argument;
-//use Ugly\DCI\Entity\User;
-//use Ugly\DCI\Factory\ContextFactory;
-//
-//class ContextAbstractSpec extends ObjectBehavior
-//{
-//
-//    function let(User $user, ContextFactory $factory)
-//    {
-//        $this->beConstructedWith($user, $factory);
-//    }
-//
-//    function it_is_initializable()
-//    {
-//        $this->shouldHaveType('Ugly\DCI\Context\ContextAbstract');
-//    }
-//
-//    function it_should_store_a_role()
-//    {
-//        $role = 'Not logged in';
-//        $this->setRole($role);
-//        $this->hasRole($role)->shouldBe(true);
-//        $this->getRole()->shouldBe($role);
-//    }
-//
-//    function it_should_store_an_entity(User $entity)
-//    {
-//        $this->setEntity($entity);
-//        $this->hasEntity()->shouldBe(true);
-//        $this->getEntity()->shouldBe($entity);
-//    }
-//}
+
+namespace spec\Ugly\DCI\Context;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Ugly\DCI\Entity\User;
+use Ugly\DCI\Factory\ContextFactory;
+
+class ContextAbstractSpec extends ObjectBehavior
+{
+
+    function let(User $user, ContextFactory $factory)
+    {
+        $this->beConstructedWith($user, $factory);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Ugly\DCI\Context\ContextAbstract');
+    }
+
+    function it_should_store_a_role()
+    {
+        $role = 'Not logged in';
+        $this->setRole($role);
+        $this->hasRole($role)->shouldBe(true);
+        $this->getRole()->shouldBe($role);
+    }
+
+    function it_should_store_an_entity(User $entity)
+    {
+        $this->setEntity($entity);
+        $this->hasEntity()->shouldBe(true);
+        $this->getEntity()->shouldBe($entity);
+    }
+}
